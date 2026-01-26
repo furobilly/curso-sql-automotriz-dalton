1f8601ddee40d8f63f6999489df2297372bafd26
furobilly <furobilly32@gmail.com>
2026-01-26 12:37:33 -0600

    Create 05_modulo_1_ejercicios.sql

---
+++ database/05_modulo_1_ejercicios.sql
@@ -0,0 +1,8 @@
+-- =============================================
+-- MÓDULO 1: FILTRADO ESTRATÉGICO
+-- Estos son tus retos para hoy.
+-- =============================================
+
+-- RETO 1: Listar solo los modelos de la marca 'Toyota'.
+-- RETO 2: Ver todos los autos que cuestan más de 500,000 pesos.
+-- RETO 3: Encontrar las ventas realizadas después del 21 de enero de 2026.
